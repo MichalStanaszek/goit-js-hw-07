91e4337c1310657abeff4e91d51731038811c5ee
MichalStanaszek <michal.stanaszek@gmail.com>
2023-06-20 22:35:58 +0200

    feat: modal pictures with slider
    still working on caption text at the bottom of picture

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,10 +3,49 @@ import { galleryItems } from "./gallery-items.js";
 const imagesGallery = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
-    (galleryItem) => `<a class="gallery__item" href="${galleryItem.original}">
+    (
+      galleryItem
+    ) => `<li><a class="gallery__item" href="${galleryItem.original}">
     <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
-  </a>
-  `
+  </a></li>`
   )
   .join("");
 imagesGallery.insertAdjacentHTML("beforeend", markup);
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {});
+
+class ModalImage extends SimpleLightbox {
+  constructor({ elements, options }) {
+    super(elements);
+    super(options);
+    this.options = Object.assign(this.defaultOptions, options);
+    this.isPassiveEventsSupported = this.checkPassiveEventsSupport();
+
+    if (typeof elements === "string") {
+      this.initialSelector = elements;
+      this.elements = Array.from(document.querySelectorAll(elements));
+    } else {
+      this.elements =
+        typeof elements.length !== "undefined" && elements.length > 0
+          ? Array.from(elements)
+          : [elements];
+    }
+
+    this.relatedElements = [];
+
+    this.transitionPrefix = this.calculateTransitionPrefix();
+    this.transitionCapable = this.transitionPrefix !== false;
+    this.initialLocationHash = this.hash;
+
+    //     if (this.options.captions && captionContainer) {
+    //       if (this.options.captionType === "data") {
+    //         captionText = captionContainer.dataset[this.options.captionsData];
+    //       } else if (this.options.captionType === "text") {
+    //         captionText = captionContainer.innerHTML;
+    //       } else {
+    //         captionText = captionContainer.getAttribute(this.options.captionsData);
+    //       }
+    //     }
+  }
+}
