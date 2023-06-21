83b3f8832fec1386c547fad85f7f4a2e74453c96
MichalStanaszek <michal.stanaszek@gmail.com>
2023-06-21 21:03:20 +0200

    refactor 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,40 +12,7 @@ const markup = galleryItems
   .join("");
 imagesGallery.insertAdjacentHTML("beforeend", markup);
 
-let gallery = new SimpleLightbox(".gallery a");
-gallery.on("show.simplelightbox", function () {});
-
-class ModalImage extends SimpleLightbox {
-  constructor({ elements, options }) {
-    super(elements);
-    super(options);
-    this.options = Object.assign(this.defaultOptions, options);
-    this.isPassiveEventsSupported = this.checkPassiveEventsSupport();
-
-    if (typeof elements === "string") {
-      this.initialSelector = elements;
-      this.elements = Array.from(document.querySelectorAll(elements));
-    } else {
-      this.elements =
-        typeof elements.length !== "undefined" && elements.length > 0
-          ? Array.from(elements)
-          : [elements];
-    }
-
-    this.relatedElements = [];
-
-    this.transitionPrefix = this.calculateTransitionPrefix();
-    this.transitionCapable = this.transitionPrefix !== false;
-    this.initialLocationHash = this.hash;
-
-    //     if (this.options.captions && captionContainer) {
-    //       if (this.options.captionType === "data") {
-    //         captionText = captionContainer.dataset[this.options.captionsData];
-    //       } else if (this.options.captionType === "text") {
-    //         captionText = captionContainer.innerHTML;
-    //       } else {
-    //         captionText = captionContainer.getAttribute(this.options.captionsData);
-    //       }
-    //     }
-  }
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
